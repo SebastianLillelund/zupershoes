da3a68f84bca58c9d34c7fae7eb9e82749a08295
BenjaminMair <bema19ac@student.cbs.dk>
2019-03-19 12:05:40 +0100

    push1
    push1

--- zuper_registration.js
+++ zuper_registration.js
@@ -1,21 +1,24 @@
 // Because I will have to perform multiple actions with the various
 // elements of our HTML form, I create a binding for each input element
 // so I will not have to use the 'document' object each time
-const firstNameUI = document.getElementById('first-name')
-const lastNameUI = document.getElementById('last-name')
-const usernameUI = document.getElementById('username') 
-const passwordUI = document.getElementById('password')
+const firstNameUI = document.getElementById('firstName')
+const lastNameUI = document.getElementById('lastName')
+const adressUI = document.getElementById('adress')
+const usernameUI = document.getElementById('userName') 
+const passwordUI = document.getElementById('userPassword')
 const checkboxUI = document.getElementById('terms')
 const submitBtnUI = document.getElementById('submit-btn')
 
 
+
+
 // To respond to the users click, I make use of the onclick property
 // of the submit button and attach a function to it
-submitBtnUI.onclick = function () {
+
+if(firstNameUI.value.length === 0) {
+    document.getElementById('resultSpan').innertext = 'Please provide username & password'
+   return false
+}
 
     console.log('clicked')
     console.log(users)
-//if(firstNameUI.value.length == 0) {
-    //document.getElementById('resultSpan').innertext = 'Please provide username & password'
-   // return false
-//}
