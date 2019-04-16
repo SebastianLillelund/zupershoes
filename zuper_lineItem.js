c6373ead8ae30b616a00dd2cf48a1754d34f6628
Sebastian <Sebastian.Lillelund@icloud.com>
2019-04-16 14:00:08 +0200

    forsøg på at genskabe før split

--- zuper_lineItem.js
+++ zuper_lineItem.js
@@ -3,24 +3,9 @@
 // Den skal så kigge efter ID og kigge efter products classen
 // Se Martins eksempel med Gin factory.
 
-class lineItem {
-    constructor(itemName, itemDescription, itemPicture){
-        for (var j = 0; j<products.length; j++) {
-            this.products[j].productName = itemName;
-            this.products[j].productDescription = itemDescription;
-            this.products[j].productPicture = itemPicture;
-        };
-    };
-}; 
 
 // click on button
  const updateCart = (index) => {
-
-//           for (var j = 0; j<products.length; j++) {
-//             // lineItem.itemName = products[j].productName
-//             // lineItem.itemDescription = products[j].productDescription
-//             // lineItem.itemPicture = products[j].productPicture
-//             };
 
 var size = document.getElementById(`size-selection-${index}`).value;
 //querySelector is a method to get an html element. Since we're using query's we're not limited to retrieving ID's.
