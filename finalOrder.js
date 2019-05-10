a3477640ef40d90bc0648aa21fb7cd3c1e7bd89c
Sebastian <Sebastian.Lillelund@icloud.com>
2019-05-10 11:22:12 +0200

    Fix - orders now only load users orders

--- finalOrder.js
+++ finalOrder.js
@@ -1,43 +1,59 @@
 // This document handles orders. All orders are saved here to be displayed historically.
 var orders = [];
 var orderHtml = '';
+var loggedUser;
 
 // method for firing on pageLoad
 document.addEventListener('DOMContentLoaded', function () {
+
     // Load in information from localStorage to var.
     orders = JSON.parse(localStorage.getItem('order'));
 
+    // Load in information from localStorage to var.
+    loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
+
+    // run loadOrders
+    loadOrders();
+});
+
+const loadOrders = () => {
     // First of all, we loop through all orders made by the user
     for (i = 0; orders.length > i; i++) {
-        // for each order made we create an order html-card element
-        orderHtml +=
 
-            // first we append the ID and date for each order.
-            `
+        // Only print the logged in users order history
+        if (orders[i].userOrder.userName === loggedUser.userName) {
+
+            // for each order made we create an order html-card element
+            orderHtml +=
+
+                // first we append the ID and date for each order.
+                `
     <div class="card">
  <h2> Your order: ${orders[i]["orderID"]} </h2>
  <p id="dateOrder-${i}"> ${orders[i]["dateOrder"]}</p>
     <body>`
 
-        // secondly, we loop through i=orders and every orders embedded array of items. 
-        // Moreover, we then loop through the items array with j
-        for (j = 0; orders[i]["itemsOrder"].length > j; j++) {
-            var item = orders[i]["itemsOrder"][j]
-            orderHtml +=
-                // lastly, we take out the productName of each item and add it to orderHtml
-                `<p id="itemsOrder-${i}">Item: ${item[1].productName} </p> `;
-        };
+            // secondly, we loop through i=orders and every orders embedded array of items. 
+            // Moreover, we then loop through the items array with j
+            for (j = 0; orders[i]["itemsOrder"].length > j; j++) {
+                var item = orders[i]["itemsOrder"][j]
+                orderHtml +=
+                    // lastly, we take out the productName of each item and add it to orderHtml
+                    `<p id="itemsOrder-${i}">Item: ${item[1].productName} </p> `;
+            };
 
-        // Final element in orderHtml is .priceOrcer and billing information stored in the order class
-        orderHtml += `
+            // Final element in orderHtml is .priceOrcer and billing information stored in the order class
+            orderHtml += `
 <p id="priceOrder-${i}">Total: DKK ${orders[i]["priceOrder"]}</p>
     <p id="userOrder-${i}">Billing: ${orders[i]["userOrder"]["firstName"]} ${orders[i]["userOrder"]["lastName"]} </p>
     <p id="userOrder-${i}">${orders[i]["userOrder"]["address"]}</p>
 
     </div>`
-    }
-    // Creating a container and appending the html var. above into the products.html after beginning.
-    var orderContainer = document.getElementById("orderContainer");
-    orderContainer.insertAdjacentHTML('beforeend', orderHtml);
+}
+};
+            // Creating a container and appending the html var. above into the products.html after beginning.
+            var orderContainer = document.getElementById("orderContainer");
+            orderContainer.insertAdjacentHTML('beforeend', orderHtml);
 
-});
+
+}
